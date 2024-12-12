fab0177df7656b57128d9935233ba298e53d8b7a
David Szankovszky <szankovszky.david@gmail.com>
2024-12-12 11:15:17 +0100

    Comments rewritten

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,13 @@ const main = async () => {
     // Meghivjuk a matrix setNumberForRow fuggvenyt, ami az osztalyon belul az aktualisan beallitott allapot megfelelo fuggvenyet fogja meghivni
     await matrix.setNumberForRow(reader);
     console.log(matrix.row);
+    // Meghivjuk a matrix setNumberForColumn fuggvenyt, ami az osztalyon belul az aktualisan beallitott allapot megfelelo fuggvenyet fogja meghivni
     await matrix.setNumberForColumn(reader);
     console.log(matrix.column);
     console.log(`Rownumbers: ${matrix.row}, columnumbers: ${matrix.column}`);
+    // Meghivjuk a matrix generateMatrix fuggvenyt, ami az osztalyon belul az aktualisan beallitott allapot megfelelo fuggvenyet fogja meghivni
     matrix.generateMatrix();
+    // Meghivjuk a matrix toString fuggvenyt, ami formazva megjeleniti a matrixunkat
     console.log(`The generated matrix:\n${matrix.toString()}`);
     process.exit(0);
 }
