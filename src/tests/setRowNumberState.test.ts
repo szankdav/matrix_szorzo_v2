6b4f1ee6e6ac9ada5e08fcadde1079f43b3b3a97
David Szankovszky <szankovszky.david@gmail.com>
2024-12-12 09:30:00 +0100

    Tests with mocking the reader function added

--- src/tests/setRowNumberState.test.ts
+++ src/tests/setRowNumberState.test.ts
@@ -7,8 +7,8 @@ import { TerminalReader } from "../classes/terminalReader";
 function mocked_reader(input: string = "") {
     let mocked_reader = {
         rl: {
-            question: (_: string, call: (answer: string) => void): void => {
-                call(input);
+            question: (_: string, callback: (answer: string) => void): void => {
+                callback(input);
             }
         }
     }
@@ -19,17 +19,65 @@ const matrix = new Matrix();
 
 describe('setRowNumberState tests', () => {
     it('should set matrix row if the given input value is valid', async () => {
-        let mocked_setRowNumberState = new SetRowNumberState(matrix);
+        let mocked_setRowNumberState: SetRowNumberState = new SetRowNumberState(matrix);
         await mocked_setRowNumberState.setNumberForRow(mocked_reader("5"))
         expect(matrix.row).equal(5)
     })
 
-    it('should call the function again if the given input is invalid', async () => {
+    it('should call self again if the given input is negative', async () => {
         let mocked_setRowNumberState: SetRowNumberState = new SetRowNumberState(matrix);
-        let mocked_reader_instance = mocked_reader();
-        let mockQuestion = vi.spyOn(mocked_reader_instance.rl, "question").mockImplementationOnce((_, call) => call("-5")).mockImplementationOnce((_, call) => call("5"))
-        await mocked_setRowNumberState.setNumberForRow(mocked_reader() as unknown as TerminalReader)
-        expect(mocked_reader_instance).toBeCalledTimes(2);
-        expect(matrix.row).equal(5);
+        let calls = 0;
+        let reader = mocked_reader();
+        const spy = vi.spyOn(mocked_setRowNumberState, "setNumberForRow")
+        reader.rl.question = vi.fn((_:string, callback: (answer: string) => void) => {
+            calls++;
+            if(calls === 1){
+                callback("-5")
+            }
+            else{
+                callback("5")
+            }
+        }) as typeof reader.rl.question
+        await mocked_setRowNumberState.setNumberForRow(reader)
+        expect(matrix.row).equal(5)
+        expect(spy).toHaveBeenCalledTimes(2)
+    })
+
+    it('should call self again if the given input is not a number', async () => {
+        let mocked_setRowNumberState: SetRowNumberState = new SetRowNumberState(matrix);
+        let calls = 0;
+        let reader = mocked_reader();
+        const spy = vi.spyOn(mocked_setRowNumberState, "setNumberForRow")
+        reader.rl.question = vi.fn((_:string, callback: (answer: string) => void) => {
+            calls++;
+            if(calls === 1){
+                callback("d")
+            }
+            else{
+                callback("5")
+            }
+        }) as typeof reader.rl.question
+        await mocked_setRowNumberState.setNumberForRow(reader)
+        expect(matrix.row).equal(5)
+        expect(spy).toHaveBeenCalledTimes(2)
+    })
+
+    it('should call self again if the given input is not one number', async () => {
+        let mocked_setRowNumberState: SetRowNumberState = new SetRowNumberState(matrix);
+        let calls = 0;
+        let reader = mocked_reader();
+        const spy = vi.spyOn(mocked_setRowNumberState, "setNumberForRow")
+        reader.rl.question = vi.fn((_:string, callback: (answer: string) => void) => {
+            calls++;
+            if(calls === 1){
+                callback("5 4")
+            }
+            else{
+                callback("5")
+            }
+        }) as typeof reader.rl.question
+        await mocked_setRowNumberState.setNumberForRow(reader)
+        expect(matrix.row).equal(5)
+        expect(spy).toHaveBeenCalledTimes(2)
     })
 })
